9524b132447136117a045c3eac947432a6f557d7
Yosr481 <yosr481@gmail.com>
2017-04-20 23:56:48 +0300

    PathFinding & AutoSteering

---
+++ Assets/Scripts/CarEngine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour {
+
+    public Transform pathHolder;
+    public float maxSteeringAngle = 45f;
+    public WheelCollider[] frontWheels = new WheelCollider[2];
+
+    Transform[] wayPoints;
+    List<Transform> nodes;
+    int currentNode = 0;
+    // Use this for initialization
+    void Start () {
+        wayPoints = pathHolder.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for(int i = 1; i < wayPoints.Length; i++)
+        {
+            nodes.Add(wayPoints[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ApplySteer();
+	}
+
+    void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        relativeVector /= relativeVector.magnitude;
+
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteeringAngle;
+        frontWheels[0].steerAngle = newSteer;
+        frontWheels[1].steerAngle = newSteer;
+    }
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+    public Color gizmosColor;
+
+    Transform[] wayPoints;
+    List<Transform> nodes;
+    // Use this for initialization
+    void Start () {
+        wayPoints = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 1; i < wayPoints.Length; i++)
+        {
+            nodes.Add(wayPoints[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update() {
+		for(int i = 0; i < nodes.Count; i++)
+        {
+            Vector3 currentNode = nodes[i].position;
+            Vector3 prevNode = Vector3.zero;
+
+            if(i > 0)
+            {
+                prevNode = nodes[i - 1].position;
+            }
+            else if(i == 0 && nodes.Count > 1)
+            {
+                prevNode = nodes[nodes.Count - 1].position;
+            }
+
+            Debug.DrawLine(prevNode, currentNode, gizmosColor);
+        }
+	}
+}
