af1e507b7985e9c3dd853a7fc4171d2ac92117f5
Yosr481 <yosr481@gmail.com>
2017-04-21 00:36:25 +0300

    Setting Motor torque

--- Assets/Scripts/CarEngine.cs
+++ Assets/Scripts/CarEngine.cs
@@ -6,7 +6,12 @@ public class CarEngine : MonoBehaviour {
 
     public Transform pathHolder;
     public float maxSteeringAngle = 45f;
-    public WheelCollider[] frontWheels = new WheelCollider[2];
+    public float maxMotorTorque = 80f;
+    public float currentSpeed;
+    public float maxSpeed = 120f;
+
+    public WheelCollider[] wheels = new WheelCollider[4];
+    public Transform[] tireMeshes = new Transform[4];
 
     Transform[] wayPoints;
     List<Transform> nodes;
@@ -21,10 +26,17 @@ public class CarEngine : MonoBehaviour {
             nodes.Add(wayPoints[i]);
         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMeshesPositions();
+    }
 	
-	// Update is called once per frame
 	void FixedUpdate () {
         ApplySteer();
+        Drive();
+        CheckWaypointDistance();
 	}
 
     void ApplySteer()
@@ -33,7 +45,51 @@ public class CarEngine : MonoBehaviour {
         relativeVector /= relativeVector.magnitude;
 
         float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteeringAngle;
-        frontWheels[0].steerAngle = newSteer;
-        frontWheels[1].steerAngle = newSteer;
+        wheels[0].steerAngle = newSteer;
+        wheels[1].steerAngle = newSteer;
+    }
+
+    void Drive()
+    {
+        currentSpeed = 2 * Mathf.PI * wheels[0].radius * wheels[0].rpm * 60 / 1000;
+
+        if(currentSpeed < maxSpeed)
+        {
+            wheels[0].motorTorque = maxMotorTorque;
+            wheels[1].motorTorque = maxMotorTorque;
+        }
+        else
+        {
+            wheels[0].motorTorque = 0;
+            wheels[1].motorTorque = 0;
+        }
+    }
+
+    void UpdateMeshesPositions()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Quaternion rotation;
+            Vector3 position;
+            wheels[i].GetWorldPose(out position, out rotation);
+
+            tireMeshes[i].position = position;
+            tireMeshes[i].rotation = rotation;
+        }
+    }
+
+    void CheckWaypointDistance()
+    {
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5f)
+        {
+            if(currentNode == nodes.Count - 1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                currentNode++;
+            }
+        }
     }
 }
