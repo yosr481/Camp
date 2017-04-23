1d0e9f492f4dc9a57012ae73fec91067380ad7f3
Yosr481 <yosr481@gmail.com>
2017-04-23 15:40:46 +0300

    Sign spawning based on bool isSpawnable;

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Sign[] signs;
+
+    public List<GameObject> signsPrefabs;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < signs.Length; i++)
+        {
+            if (signs[i].isSpawnable)
+            {
+                signsPrefabs.Add(signs[i].gameObject);
+                Debug.Log(signs[i].gameObject.name + " is spawnable");
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+	}
+}
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sign : MonoBehaviour {
-
+    [Tooltip("Used to organized the questions by the sign category")]
     public string signCategory;
+    public List<Question> questions;
+    public bool isSpawnable =false;
 
     QuestionHolder questionHolder;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         questionHolder = GameObject.FindObjectOfType<QuestionHolder>();
-	}
+
+        FindAndAddQuestions();
+        CheckIfSpawnable();
+    }
+
+    void FindAndAddQuestions()
+    {
+        foreach (Question q in questionHolder.allQuestions)
+        {
+            if (q.category == signCategory)
+            {
+                questions.Add(q);
+            }
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void CheckIfSpawnable()
+    {
+        if (questions.Count > 0)
+            isSpawnable = true;
+        else
+            isSpawnable = false;
+    }
 }
